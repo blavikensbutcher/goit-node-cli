c3650ed2dab47cf457fa6d75f83cb612f9e2267b
blavikensbutcher <luckertheory@gmail.com>
2024-03-22 21:06:51 +0200

    +add try catch

--- contacts.js
+++ contacts.js
@@ -6,58 +6,53 @@ const contactsPath = path.resolve("db", "contacts.json");
 export async function listContacts() {
   try {
     const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
   } catch (e) {
     console.log(e.message);
   }
-  return JSON.parse(data);
 }
 
 export async function getContactById(contactId) {
   try {
     const data = await fs.readFile(contactsPath);
+    const dataNormalized = JSON.parse(data);
+    return dataNormalized.filter((item) => item.id === contactId);
   } catch (e) {
     console.log(e.message);
   }
-
-  const dataNormalized = JSON.parse(data);
-  return dataNormalized.filter((item) => item.id === contactId);
 }
 
 export async function removeContact(contactId) {
   try {
     const data = await fs.readFile(contactsPath);
+    const dataNormalized = JSON.parse(data);
+    const isExist = dataNormalized.some((contact) => contact.id === contactId);
+    if (isExist) {
+      return dataNormalized.filter((item) => item.id === contactId)[0];
+    } else {
+      return null;
+    }
   } catch (e) {
     console.log(e.message);
-  }
-  const dataNormalized = JSON.parse(data);
-  const isExist = dataNormalized.some((contact) => contact.id === contactId);
-
-  if (isExist) {
-    return dataNormalized.filter((item) => item.id === contactId)[0];
-  } else {
-    return null;
   }
 }
 
 export async function addContact(name, email, phone) {
   try {
     const data = await fs.readFile(contactsPath);
-  } catch (e) {
-    console.log(e.message);
-  }
-  const dataArray = JSON.parse(data);
-  const newUser = {
-    id: String(Date.now()),
-    name,
-    email,
-    phone,
-  };
-  dataArray.push(newUser);
-  try {
-    await fs.writeFile(contactsPath, JSON.stringify(dataArray));
-  } catch (e) {
-    console.log(e.message);
-  }
+    const dataArray = JSON.parse(data);
+    const newUser = {
+      id: String(Date.now()),
+      name,
+      email,
+      phone,
+    };
+    dataArray.push(newUser);
 
-  return newUser;
+    await fs.writeFile(contactsPath, JSON.stringify(dataArray));
+
+    return newUser;
+  } catch (e) {
+    console.log(e.message);
+  }
 }
